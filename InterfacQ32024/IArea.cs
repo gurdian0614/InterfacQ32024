3fb863f590e2542cc7512149838c04aa5bcc2322
gurdian0614 <ivan.gurdian@outlook.com>
2024-07-24 20:48:41 -0600

    Creando interfaz Area

---
+++ InterfacQ32024/IArea.cs
@@ -0,0 +1,21 @@
+﻿
+namespace InterfacQ32024
+{
+    /// <summary>
+    /// Interfaz que contiene los métodos para calcular diferentes áreas matemáticas
+    /// </summary>
+    public interface IArea
+    {
+        /// <summary>
+        /// Implementa el proceso de calcular el área de un cuadrado
+        /// </summary>
+        /// <returns>Area del cuadrado</returns>
+        public double Cuadrado();
+
+        /// <summary>
+        /// Implementa el proceso de calcular el área de un triángulo
+        /// </summary>
+        /// <returns>Area del triángulo</returns>
+        public double Triangulo();
+    }
+}

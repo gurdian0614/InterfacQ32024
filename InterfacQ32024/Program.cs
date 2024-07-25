cf0aca5a05f9b8c64ae347a32ccf1e94bcd58fdf
gurdian0614 <ivan.gurdian@outlook.com>
2024-07-24 20:49:21 -0600

    Funcionalidad del programa

--- InterfacQ32024/Program.cs
+++ InterfacQ32024/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using InterfacQ32024;
+
+Operacion operacion = new Operacion();
+Console.WriteLine("Ingrese primer número:");
+bool validN1 = double.TryParse(Console.ReadLine(), out operacion.N1);
+
+Console.WriteLine("Ingrese segundo número:");
+bool validN2 = double.TryParse(Console.ReadLine(), out operacion.N2);
+
+Console.WriteLine();
+if (validN1 && validN2)
+{
+    if (operacion.N2 != 0)
+    {
+        operacion.Imprimir();
+    } else
+    {
+        Console.WriteLine("Segundo valor no puede ser cero");
+    }
+} else
+{
+    Console.WriteLine("Uno de los valores o los valores ingresados no tiene el formato correcto...");
+}

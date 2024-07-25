e6366910592a8705b1800375a426043a6c97db48
gurdian0614 <ivan.gurdian@outlook.com>
2024-07-24 20:48:51 -0600

    Creando interfaz operacion

---
+++ InterfacQ32024/IOperacion.cs
@@ -0,0 +1,34 @@
+﻿
+namespace InterfacQ32024
+{
+    /// <summary>
+    /// Interfaz que contiene los métodos para calcular las diferentes operaciones básicas
+    /// </summary>
+    public interface IOperacion
+    {
+        /// <summary>
+        /// Implementa el proceso de una suma
+        /// </summary>
+        /// <returns>Total de la suma</returns>
+        public double Suma();
+
+        /// <summary>
+        /// Implementa el proceso de una resta
+        /// </summary>
+        /// <returns>Total de la resta</returns>
+        public double Resta();
+
+        /// <summary>
+        /// Implementa el proceso de una multiplicación
+        /// </summary>
+        /// <returns>Total de la multiplicación</returns>
+        public double Multiplicacion();
+
+        /// <summary>
+        /// Implementa el proceso de una división
+        /// </summary>
+        /// <returns>Total de la división</returns>
+        public double Division();
+
+    }
+}

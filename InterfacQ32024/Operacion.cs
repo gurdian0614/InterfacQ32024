847973ac488661b106631195e7780eaf27b6becc
gurdian0614 <ivan.gurdian@outlook.com>
2024-07-24 20:49:08 -0600

    Implementando interfaces en clase operacion

---
+++ InterfacQ32024/Operacion.cs
@@ -0,0 +1,50 @@
+﻿
+namespace InterfacQ32024
+{
+    public class Operacion : IOperacion, IArea
+    {
+        public double N1;
+
+        public double N2;
+
+        public double Division()
+        {
+            return N1 / N2;
+        }
+
+        public double Multiplicacion()
+        {
+            return N1 * N2;
+        }
+
+        public double Resta()
+        {
+            return N1 - N2;
+        }
+
+        public double Suma()
+        {
+            return N1 + N2;
+        }
+
+        public double Cuadrado()
+        {
+            return N1 * N2;
+        }
+
+        public double Triangulo()
+        {
+            return N1 * N2 / 2;
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine($"La Suma es: {Suma()}");
+            Console.WriteLine($"La Resta es: {Resta()}");
+            Console.WriteLine($"La Multiplicación es: {Multiplicacion()}");
+            Console.WriteLine($"La División es: {Division()}");
+            Console.WriteLine($"El área del cuadrado es: {Cuadrado()}");
+            Console.WriteLine($"El área del triángulo es: {Triangulo()}");
+        }
+    }
+}
